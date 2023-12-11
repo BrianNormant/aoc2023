1cd68884446390ec53aa6ae6318c8ce4b74e3a5e
Brian Normant <briannormant@gmail.com>
2023-12-11 15:30:24 -0500

    better default.c

--- default/main.c
+++ default/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define FILE_NAME "example.txt"
 #define MAX_LINE_LEN 1000
@@ -7,7 +8,7 @@
 void part1(FILE *fp);
 void part2(FILE *fp);
 
-int main(int argc, char *argv[]) {
+int main(void) {
         FILE *fp;
 
         fp = fopen(FILE_NAME, "r");
@@ -31,5 +32,16 @@ int main(int argc, char *argv[]) {
 }
 
 
-void part1(FILE *fp) {}
+void part1(FILE *fp) {
+        char line_buf[MAX_LINE_LEN];
+        char *line = line_buf;
+
+        int solution = 0;
+
+        while (fgets(line, MAX_LINE_LEN, fp)!= NULL) {
+                line = line_buf;
+        }
+
+        printf("Solution: %d\n", solution);
+}
 void part2(FILE *fp) {}
